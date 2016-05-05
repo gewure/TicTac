977e3d35f62b9e7f0b81312b06c3f1c4e4fc9ff0
Norskan <simon.angerer@hotmail.com>
2016-05-05 14:03:33 +0200

    finished game loop. transformation from input detection to GamePositions still missing

--- Application/src/app/GameController.java
+++ Application/src/app/GameController.java
@@ -3,6 +3,7 @@ package app;
 import Gateway.GameFacade;
 import Gateway.GamePosition;
 import Gateway.GameToken;
+import Logic.Phase;
 import Logic.Token;
 import at.fhv.itb6.arp.ARFacade;
 
@@ -17,6 +18,7 @@ public class GameController extends Observable implements Runnable{
 
     public GameFacade _gameFacade;
 
+    private boolean _gameOver;
     public GameController(Integer cameraID, GameType gameType) {
         _gameFacade = new GameFacade();
         _gameFacade.addObserver(new Observer() {
@@ -26,41 +28,55 @@ public class GameController extends Observable implements Runnable{
             }
         });
 
-        ARFacade.getInstance().init(cameraID, _gameFacade);
+        ARFacade.getInstance().init(cameraID);
 
         start();
-
-        _gameFacade.placeGameToken(GamePosition.Center0);
-
-        _gameFacade.placeGameToken(GamePosition.Center1);
-
-        _gameFacade.placeGameToken(GamePosition.Center2);
     }
 
     private void start() {
-        //start thread that runs the game
         Thread gameLoopThread = new Thread(this);
-
         gameLoopThread.start();
     }
 
     private boolean _running;
     @Override
     public void run() {
-        _running = false; //TODO: set to true
+        _running = true;
         while(_running) {
-            //read cursor input
+            actOnPhase(_gameFacade.getCurrentPhase());
 
-            //write it into the gamegateway
+            if(_gameFacade.getWonPlayer() != Token.NO_PLAYER) {
+                _gameOver = true;
+            }
 
             gameStateChanged();
-
-            // if player won stop loop
         }
+    }
+
+    private void actOnPhase(Phase phase) {
+        GamePosition src = readeInput();
+        if(phase == Phase.MOVING_PLAYER1 || phase == Phase.MOVING_PLAYER2) {
+            GamePosition dest = readeInput();
+            _gameFacade.moveGameToken(src, dest);
+
+        } else if(phase == Phase.REMOVING_PLAYER1 || phase == Phase.REMOVING_PLAYER2) {
+            _gameFacade.removeGameToken(src);
+
+        } else if(phase == Phase.PLACING_PLAYER1 || phase == Phase.PLACING_PLAYER2) {
+            _gameFacade.placeGameToken(src);
+        }
+    }
+
+    private GamePosition readeInput(){
+        return GamePosition.Center0;
     }
 
     public Token getCurrentPlayer() {
         return _gameFacade.getCurrentPlayer();
+    }
+
+    public boolean getGameOver() {
+        return _gameOver;
     }
 
     public List<GameToken> getPlayer1GameTokens() {
@@ -72,7 +88,7 @@ public class GameController extends Observable implements Runnable{
     }
 
     private void gameStateChanged() {
-        this.hasChanged();
+        this.setChanged();
         this.notifyObservers();
     }
 
